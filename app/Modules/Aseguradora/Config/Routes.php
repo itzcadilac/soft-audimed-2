6179ea3bb7aec2ca7b201097264342e77e9d841f
Richard Juarez <patrick69ns@gmail.com>
2025-02-02 21:05:20 -0500

    Modulo aseguradora se agregan carpetas

--- app/Modules/Aseguradora/Config/Routes.php
+++ app/Modules/Aseguradora/Config/Routes.php
@@ -4,7 +4,7 @@ namespace Modules\Aseguradora\Config;
 
 use CodeIgniter\Router\RouteCollection;
 
-const USER_MODULE_NAMESPACE = 'Modules\Aseguradora\Infrastructure\In\Controller';
+const ASEG_MODULE_NAMESPACE = 'Modules\Aseguradora\Infrastructure\In\Controller';
 
 /**
  * Define rutas específicas para el módulo de usuarios.
@@ -14,7 +14,7 @@ const USER_MODULE_NAMESPACE = 'Modules\Aseguradora\Infrastructure\In\Controller'
 
 function AseguradoraRoutes(RouteCollection $routes)
 {
-    $routes->group('aseguradora', ['namespace' => USER_MODULE_NAMESPACE], function ($routes) {
+    $routes->group('aseguradora', ['namespace' => ASEG_MODULE_NAMESPACE], function ($routes) {
         $routes->get('getAsegxUser', 'AseguradoraController::getAseguradoraxUser');
         $routes->get('getTest', 'AseguradoraController::getTest');
         //$routes->post('registro', 'UserRegisterController::registerAction');
